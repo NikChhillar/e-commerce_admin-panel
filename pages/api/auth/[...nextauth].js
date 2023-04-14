944d09df3ae9f12f4e8a092129b49ed3543f736c
Nikhil <nikhilchhillar29@gmail.com>
2023-04-14 15:10:01 +0530

    google auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,16 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import clientPromise from "@/lib/mongodb";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-    providers: [
-        // OAuth authentication providers...
-    
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET
-        }),
-      
-    ]
-})
+  providers: [
+    // OAuth authentication providers...
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+});
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg">
-          Login with google
-        </button>
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white p-2 px-4 rounded-lg"
+          >
+            Login with google
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <div>logged in {session.user.email}</div>;
 }
